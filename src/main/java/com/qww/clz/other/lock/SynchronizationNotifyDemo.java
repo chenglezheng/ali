94c6b9923eafe6e5878dfc9321c2b9a7b9a11f61
chenglezheng <981797842@qq.com>
2020-11-09 16:32:12 +0800

    线程通知测试

--- src/main/java/com/qww/clz/other/lock/SynchronizationNotifyDemo.java
+++ src/main/java/com/qww/clz/other/lock/SynchronizationNotifyDemo.java
@@ -9,36 +9,38 @@ class ShareResource1 {
 
     private Integer ticketNumber=0;
 
+    private final static int MAX_VALUE=100;
+
 
     public void concumer() throws InterruptedException {
         synchronized (lock1) {
             while (ticketNumber<=0) {
+                /*System.out.println("等待，释放锁，"+Thread.currentThread().getName()+"进入锁等待池!");*/
                 lock1.wait();
             }
-            System.out.println("ticketNumber:"+ticketNumber+"开始消费了");
-            ticketNumber=0;
+            ticketNumber-=10;
             try {
                 TimeUnit.SECONDS.sleep(2);
             } catch (Throwable e) {
             }
-            System.out.println("ticketNumber:"+ticketNumber+"消费完了");
-            lock1.notify();
+            System.out.println("消费完，库里已有"+ticketNumber+"产品!");
+            lock1.notifyAll();
         }
     }
 
     public void producer() throws InterruptedException {
         synchronized (lock1) {
-            while (ticketNumber>10) {
+            while (ticketNumber>MAX_VALUE) {
+                /*System.out.println("等待，释放锁，"+Thread.currentThread().getName()+"进入锁等待池!");*/
                 lock1.wait();
             }
-            System.out.println("ticketNumber:"+ticketNumber+"开始生产了");
             try {
                 TimeUnit.SECONDS.sleep(2);
             } catch (Throwable e) {
             }
             ticketNumber+=10;
-            System.out.println("ticketNumber:"+ticketNumber+"生产完了");
-            lock1.notify();
+            System.out.println("生产完，库里已有个"+ticketNumber+"产品！");
+            lock1.notifyAll();
         }
 
     }
@@ -55,7 +57,7 @@ public class SynchronizationNotifyDemo {
 
     public static void main(String[] args) {
         ExecutorService executorService = new ThreadPoolExecutor(
-                Runtime.getRuntime().availableProcessors() / 2,
+                Runtime.getRuntime().availableProcessors()/2,
                 Runtime.getRuntime().availableProcessors() + 1
                 , 60,
                 TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(Runtime.getRuntime().availableProcessors() * 4),
@@ -63,19 +65,23 @@ public class SynchronizationNotifyDemo {
 
         ShareResource1 shareResource1 = new ShareResource1();
         try{
-            for (int i = 0; i <5 ; i++) {
+            System.out.println("用户开始抢票！");
+            for (int i = 0; i <3 ; i++) {
                 executorService.submit(()->{
                     try{
-                        shareResource1.producer();
+                        shareResource1.concumer();
                     }catch(Exception e){
 
                     }
                 });
             }
-            for (int i = 0; i <5 ; i++) {
+            System.out.println("高铁站还没放票，等着放票...");
+            try{ TimeUnit.SECONDS.sleep(5); }catch(Throwable e){}
+            System.out.println("高铁站开始放票!");
+            for (int i = 0; i <3 ; i++) {
                 executorService.submit(()->{
                     try{
-                        shareResource1.concumer();
+                        shareResource1.producer();
                     }catch(Exception e){
 
                     }
